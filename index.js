5b2208af667a5b19eb63bf3a91fae8407ab87c9d
Umar_Farouk <yasmarfaq@yahoo.com>
2022-11-02 09:08:58 +0100

    added some routes inthe ndex.js file

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ const routes = require("./routes/flightRoute");
 
 const app = express();
 
+// const flight = [
+//   {
+//     title: "flight to canada",
+//     time: 1,
+//     price: 26000,
+//     date: "26-06-2022"
+//   }
+// ];
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.get('/api/flights', (req, res) => {
+  res.send(flights)
+})
+
+//this is routes for creating single flights
+app.get('/api/flights/:id', (req, res) => {
+  const flights = flights.find(f => f.id === parseInt(req.params.id));
+  if (!flights) res.status(404).send('the flights with the given ID is not found');
+  res.send(flights);
+});
+ 
 app.use(json());
 
 app.use("/", routes);
